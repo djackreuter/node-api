5e03495fa4774c60c60046e14e2ec2430595d47f
Jack Reuter <djreuter45@gmail.com>
2018-07-26 13:33:24 -0600

    added test for get route and fixed post method

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
-  todo.save((doc) => {
+  todo.save().then((doc) => {
     res.send(doc);
   }, (err) => {
     res.status(400).send(err);

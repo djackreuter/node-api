f4ae926c5d407a9ff009b08c42348a4e79209de5
Jack Reuter <djreuter45@gmail.com>
2018-07-26 09:19:19 -0600

    added post route

--- server/server.js
+++ server/server.js
@@ -5,3 +5,20 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(3000, () => console.log('Started server on port 3000...'));
+

68b3de4c1d4e22f977abd701a2a27d63505724d1
Jack Reuter <djreuter45@gmail.com>
2018-07-31 10:23:21 -0600

    added bcryptjs and hashing user password before saving

--- testing/hashing.js
+++ testing/hashing.js
@@ -1,17 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-};
+var password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+var hashedPassword = '$2a$10$jMUjdMEs3jmm/USLxPDXheHRDniai85OtVmsUJ96XOzk.xCBWdVs.';
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
-let decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-let message = 'I am user no 3';
-let hash = SHA256(message).toString();
+// let data = {
+//   id: 10
+// };
+
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
+// let decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
+
+// let message = 'I am user no 3';
+// let hash = SHA256(message).toString();
 
 // console.log(`message: ${message}`);
 // console.log(`message: ${hash}`);

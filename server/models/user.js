1458d0af546258c2d99148b2cf453fcf41e45300
Jack Reuter <djreuter45@gmail.com>
2018-07-30 20:15:20 -0600

    modified user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,48 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minLength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    },
+  },
+  password: {
+    type: String, 
+    require: true,
+    minLength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String, 
+      required: true
+    }
+  }]
 });
+
+UserSchema.methods.generateAuthToken = function() {
+  // let user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: this._id.toHexString(), access}, 'abc123').toString();
+  this.tokens.push({
+    access,
+    token
+  });
+  return this.save().then(() => token);
+}
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
   User

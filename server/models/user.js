b5694aa5753d820f31dd8fa30e0d62e1ef075c32
Jack Reuter <djreuter45@gmail.com>
2018-07-31 09:32:57 -0600

    added auth middleware, users/me route

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -31,6 +32,11 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
+UserSchema.methods.toJSON = function() {
+  var userObj = this.toObject();
+  return _.pick(userObj, ['_id', 'email']);
+}
+
 UserSchema.methods.generateAuthToken = function() {
   // let user = this;
   var access = 'auth';
@@ -40,6 +46,23 @@ UserSchema.methods.generateAuthToken = function() {
     token
   });
   return this.save().then(() => token);
+}
+
+UserSchema.statics.findByToken = function(token) {
+  var decoded;
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch(err) {
+    // return Promise.reject();
+    return new Promise((resolve, reject) => {
+      reject();
+    });
+  }
+  return this.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
 }
 
 var User = mongoose.model('User', UserSchema);

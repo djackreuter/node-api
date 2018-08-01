eefd91533b31af6d1494768b299802ccd5239029
Jack Reuter <djreuter45@gmail.com>
2018-08-01 13:26:05 -0600

    added logout route

--- server/models/user.js
+++ server/models/user.js
@@ -49,6 +49,14 @@ UserSchema.methods.generateAuthToken = function() {
   return this.save().then(() => token);
 }
 
+UserSchema.methods.removeToken = function(token) {
+  return this.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+}
+
 UserSchema.statics.findByToken = function(token) {
   var decoded;
   try {
--- server/server.js
+++ server/server.js
@@ -109,6 +109,14 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => console.log(`Started server on port ${port}...`));
 
 module.exports = {app}

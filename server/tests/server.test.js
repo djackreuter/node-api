c95a32365e6027ea5f505fce7207fb37b5e389f8
Jack Reuter <djreuter45@gmail.com>
2018-08-01 12:57:27 -0600

    added tests for login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -206,7 +206,7 @@ describe('POST /users', () => {
           expect(user).toBeTruthy();
           expect(user.password).not.toBe(password);
           done();
-        });
+        }).catch((err) => done(err));
       });
   });
 
@@ -230,5 +230,44 @@ describe('POST /users', () => {
     })
     .expect(400)
     .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeTruthy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toHaveProperty('access', 'auth');
+          expect(user.tokens[0]).toHaveProperty('token', res.headers['x-auth']);
+        });
+        done();
+      }).catch((err) => done());
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password + '123'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeFalsy();
+      })
+      .end(done);
   });
 });

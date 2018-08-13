af1bfc847d89992bbc79cd3963e03e64c1ba5a1d
Jack Reuter <djreuter45@gmail.com>
2018-08-13 09:28:19 -0600

    refactored some promises to use async await

--- server/server.js
+++ server/server.js
@@ -112,25 +112,28 @@ app.post('/users', (req, res) => {
   }).catch((err) => res.status(400).send(err));
 });
 
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  User.findByCredentials(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
-    });
-  }).catch((err) => res.status(400).send());
+app.post('/users/login', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (err) {
+    res.status(400).send();
+  }
 });
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token);
     res.status(200).send();
-  }, () => {
+  } catch (err) {
     res.status(400).send();
-  });
+  }
 });
 
 app.listen(port, () => console.log(`Started server on port ${port}...`));
